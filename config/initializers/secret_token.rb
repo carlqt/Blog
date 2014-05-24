acbdc87932c58c428969f9b3aba3a371dabae862
Carl William Tablante <carlwilliamtablante@Carls-MacBook-Air.local>
2014-05-24 13:46:36 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NewFuratto::Application.config.secret_key_base = 'bc42c5761787585f8d3278778571714987d4d4edbe4e1f2aa387a9beab688ec5d552e4b3025ace6757bb7442705a2c03b28bed2a229ab9d845cc1340fe8ceffd'
